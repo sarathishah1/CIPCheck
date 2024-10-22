ce164c0430124d9de588b09799f94131150a70ae
sarathishah1 <126651984+sarathishah1@users.noreply.github.com>
2024-10-22 11:57:15 +0530

    Update helper_function.py

--- helper_function.py
+++ helper_function.py
@@ -12,11 +12,11 @@ def create_base_encoding():
   # # Define the predefined statements
   # statements = df['Intermediate Description'].tolist()
   # # Compute embeddings Afor all sentences
-  # statement_embeddings = model.encode(statements)
-  np_load_old = np.load
-  # modify the default parameters of np.load
-  np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
-  statement_embeddings=np.load('embeddings.npy')
+  # # statement_embeddings = model.encode(statements)
+  # np_load_old = np.load
+  # # modify the default parameters of np.load
+  # np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+  statement_embeddings=np.load('embeddings.npy',allow_pickle=True)
   return statement_embeddings
 
 def create_input_encoding(input_statement,baseencoding):
