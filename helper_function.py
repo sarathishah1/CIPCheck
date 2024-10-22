f0bdf1c81f9fb5d1eac68bd69c329a15190a5c3e
sarathishah1 <126651984+sarathishah1@users.noreply.github.com>
2024-10-22 15:57:57 +0530

    Update helper_function.py

--- helper_function.py
+++ helper_function.py
@@ -16,8 +16,8 @@ def create_base_encoding():
   # np_load_old = np.load
   # # modify the default parameters of np.load
   # np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
-  statement_embeddings=np.load('embeddings.npy',allow_pickle=True)
-  statement_embeddings = statement_embeddings.astype(np.float32)
+  statement_embeddings=np.load('base_encoding.npy',allow_pickle=True)
+  #statement_embeddings = statement_embeddings.astype(np.float32)
   return statement_embeddings
 
 def create_input_encoding(input_statement,baseencoding):
